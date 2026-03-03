37f1f9224cd71985548d36834e65444a35a31945
shafaullahagha <shafaullahagha@gmail.com>
2026-03-03 14:50:30 +0500

    Add docker_push function for pushing Docker images

---
+++ vars/docker_push.groovy
@@ -0,0 +1,13 @@
+def call(String credId, String image, String ImageTag){
+  withCredentials([usernamePassword(
+                    credentialsId: "${credId}",
+                    usernameVariable: "DOCKER_USER",
+                    passwordVariable: "DOCKER_PASS"
+                )]) {
+
+                    sh """
+                    echo "$DOCKER_PASS" | docker login -u "$DOCKER_USER" --password-stdin
+                    docker push ${image}:${ImageTag}
+                    """
+                }
+}
